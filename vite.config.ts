e6fae69f64a6a8d3b7ec686b657461bac08c482f
Johannes Kärnstam <johannes.karnstam@vgregion.se>
2026-06-25 13:36:37 +0200

    mc

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+        name: 'PushIt',
+        short_name: 'PushIt',
+        description: 'Advanced Tally Counter PWA',
+        theme_color: '#0a0a0f',
+        icons: [
+          {
+            src: 'img/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'img/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
